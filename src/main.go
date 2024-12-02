37d5b69979bfb69ea1f272a2f3392b3fe1cafbbe
Matt Mets <matt@blinkinlabs.com>
2024-12-02 03:36:29 +0100

    Switch to usb-to-serial demo

--- src/main.go
+++ src/main.go
@@ -1,38 +1,77 @@
-//https://tinygo.org/docs/reference/microcontrollers/pico/
-
 package main
 
 import (
         "machine"
 //        "time"
-//        "fmt"
+        "fmt"
+)
+
+var (
+    uart = machine.UART1
+    usbcdc = machine.USBCDC
 )
 
 func main() {
+    fmt.Println("USB to Serial converter demo\r\n")
+
+    pin := machine.GPIO7
+    pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+    pin.High()
+
+
+    c := machine.UARTConfig{
+        BaudRate: 62500,
+        TX : machine.UART1_TX_PIN,
+        RX : machine.UART1_RX_PIN,
+    }
+    uart.Configure(c)
+
 
     led := machine.LED
     led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
+    led.High()
+
     for {
-        led.Low()
-        led.High()
+        // Transmit data from USB to UART
+        if usbcdc.Buffered() > 0 {
+            led.Low()
+
+            data, err := usbcdc.ReadByte()
+
+            if err == nil {
+                uart.WriteByte(data)
+            }
+
+            led.High()
+        }
+
+        // Transmit data from UART to USB
+        if uart.Buffered() > 0 {
+            led.Low()
+
+            data, err := uart.ReadByte()
+
+            if err == nil {
+                usbcdc.WriteByte(data)
+            }
+
+            led.High()
+        }
     }
 
 //    count := 0
 //    for {
+//        led.Low()
 //        fmt.Println("Hello world!", count)
+//        usbcdc.WriteByte('a')
+//        
+//        time.Sleep(500*time.Millisecond)
+//
+//        led.High()
+//        fmt.Println('y')
+//        time.Sleep(500*time.Millisecond)
+//
 //        count++
-//
-//        led.Low()
-//        time.Sleep(time.Millisecond * 200)
-//
-//        led.High()
-//        time.Sleep(time.Millisecond * 1000)
-//
-//        led.Low()
-//        time.Sleep(time.Millisecond * 200)
-//
-//        led.High()
-//        time.Sleep(time.Millisecond * 200)
 //    }
 }
